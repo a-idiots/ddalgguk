310d11e40c25f0f7c61b66083d4ae5959f260f0d
Eunwoo Choi <soarhigh03@gmail.com>
2026-04-05 17:23:49 +0900

    fix: Fit the horizontal width of 'drinking guideline' widget

--- lib/features/profile/widgets/detail_screen/drinking_guideline_section.dart
+++ lib/features/profile/widgets/detail_screen/drinking_guideline_section.dart
@@ -83,6 +83,7 @@ class _GuidelineCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
       decoration: BoxDecoration(
